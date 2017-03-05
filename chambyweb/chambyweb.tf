bed0d4eb9eb7320c6a37fad71ff55805898b7226
Brad Chamberlain <chamby@gmail.com>
2017-03-05 06:47:33 -0500

    moved config to variables

--- chambyweb/chambyweb.tf
+++ chambyweb/chambyweb.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_elb" "chambyweb-elb" {
   name               = "chambyweb-elb"
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  availability_zones = "${var.availability_zones}"
 
   listener {
     instance_port     = 80
@@ -26,11 +26,11 @@ resource "aws_elb" "chambyweb-elb" {
 }
 
 resource "aws_autoscaling_group" "chambyweb-asg" {
-  availability_zones        = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  availability_zones        = "${var.availability_zones}"
   name                      = "chambyweb-us-east"
-  max_size                  = 4
-  min_size                  = 2
-  desired_capacity          = 2
+  min_size                  = "${var.asg_min_size}"
+  max_size                  = "${var.asg_max_size}"
+  desired_capacity          = "${var.asg_desired_size}"
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.chambyweb-lc.name}"
   load_balancers            = ["${aws_elb.chambyweb-elb.name}"]
@@ -38,10 +38,10 @@ resource "aws_autoscaling_group" "chambyweb-asg" {
 
 resource "aws_launch_configuration" "chambyweb-lc" {
   name            = "chambyweb-lc"
-  image_id        = "ami-82a67294"
-  instance_type   = "t2.micro"
+  image_id        = "${var.ami_id}"
+  instance_type   = "${var.instance_type}"
   security_groups = ["${aws_security_group.allow-http.id}"]
-  key_name        = "chambyweb"
+  key_name        = "${var.key_name}"
 }
 
 resource "aws_security_group" "allow-http" {
@@ -49,9 +49,23 @@ resource "aws_security_group" "allow-http" {
   description = "Allow HTTP traffic to LB"
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["68.197.153.223/32"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -62,21 +76,14 @@ resource "aws_security_group" "allow-http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags {
     Name = "chambyweb"
   }
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = "Z36Z4PT7NFJUY"
-  name    = "www.chamby.io"
+  zone_id = "${var.dns_zone_id}"
+  name    = "${var.dns_www_name}"
   type    = "CNAME"
   ttl     = "300"
   records = ["${aws_elb.chambyweb-elb.dns_name}"]
--- chambyweb/variables.tf
+++ chambyweb/variables.tf
@@ -3,3 +3,20 @@ variable "secret_key" {}
 variable "region" {
   default = "us-east-1"
 }
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+variable "asg_min_size" {
+  default = 2
+}
+variable "asg_max_size" {
+  default = 4
+}
+variable "asg_desired_size" {
+  default = 2
+}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "dns_zone_id" {}
+variable "dns_www_name" {}
